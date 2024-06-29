f46ec8e1bddede4e6173e8fa2a9605a3f82f6ddc
Davi Remedio Pereira <remediodavi009@gmail.com>
2024-06-29 13:46:43 -0300

    Hello World

---
+++ Hello-World.py
@@ -0,0 +1,6 @@
+#print is a type of command used for output informations on the screen
+#the quote is used to transform a data in string
+print('Hello World')
+#when I want to use raw data calculation to be displayed on screen, I can't use quote marks
+print(7+4)
+print('7' + '4')
