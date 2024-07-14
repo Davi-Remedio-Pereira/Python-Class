fd27adab5467e8074338db20930c6cd77f11acd7
Davi Remedio Pereira <remediodavi009@gmail.com>
2024-07-14 09:54:49 -0300

    Update ex019.py

--- exercices/ex019.py
+++ exercices/ex019.py
@@ -1,7 +1,9 @@
 #A teacher wants to draw one of his four students to erase the board. Make a program that helps them, reading their name and writing that of the chosen one
 import random
-list = []
-choice = random.randint(1,4)
-while len(list) != 4:
-    list = input("Student Name: ")
-print("The chosen one is...{}!" .format())
+n1 = str(input("First Student: "))
+n2 = str(input("Second Student: "))
+n3 = str(input("Third Student: "))
+n4 = str(input("Fourth Student: "))
+list = [n1, n2, n3, n4]
+choice = random.choice(list)
+print("The chosen one is...{}!" .format(choice))
